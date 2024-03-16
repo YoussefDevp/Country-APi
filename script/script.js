4c6d024d2f66c88b99b0bf2367c8efdb4e3c5f2f
YoussefDevp <youssefmed212@gmail.com>
2024-03-16 03:16:47 +0100

    fetch country from api v1.0

--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
 fetch('./data.json')
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then(data => {
+        // Create HTML elements for each item
+        data.forEach(item => {
+        var div = document.createElement('div');
+        div.textContent = `
+        Name: ${item.name},
+        Population: ${item.population},
+        region: ${item.region} ,
+        capital: ${item.capital},
+        `;
+        // Append the created element to the container
+        document.getElementById('countrys').appendChild(div);
+    });
+})
