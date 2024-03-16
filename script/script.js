176401685c7cd84fbfe9ebed2a7ee61538bdfe6e
YoussefDevp <youssefmed212@gmail.com>
2024-03-16 04:24:17 +0100

    fetch country from api v1.2

--- script/script.js
+++ script/script.js
@@ -1,16 +1,36 @@
+const container = document.getElementById('countrys')
 fetch('./data.json')
     .then((response) => response.json())
     .then(data => {
-        // Create HTML elements for each item
-        data.forEach(item => {
-        var div = document.createElement('div');
-        div.textContent = `
-        Name: ${item.name},
-        Population: ${item.population},
-        region: ${item.region} ,
-        capital: ${item.capital},
-        `;
-        // Append the created element to the container
-        document.getElementById('countrys').appendChild(div);
+        data.forEach(item => {     
+            let div = document.createElement('div');
+            let countryData = document.createElement('div');
+
+            let heading = document.createElement('h2');
+            heading.textContent = item.name;
+
+            let population = document.createElement('p');
+            population.textContent = item.population;
+
+            let region = document.createElement('p');
+            region.textContent = item.region;
+
+            let capital = document.createElement('p');
+            capital.textContent = item.capital;
+
+            let img = document.createElement('img');
+            img.setAttribute('src', item.flag);
+            img.setAttribute('alt', item.name);
+
+            div.appendChild(img);
+            div.appendChild(countryData)
+            
+            countryData.classList.add("countryData");
+            countryData.appendChild(heading); 
+            countryData.appendChild(population); 
+            countryData.appendChild(region); 
+            countryData.appendChild(capital); 
+            container.appendChild(div);   
     });
 })
+
